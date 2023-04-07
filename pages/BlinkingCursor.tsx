b7806fa486882290733a660a125e91f05c15ff87
TravisBoyd884 <phtboyd884@gmail.com>
2023-04-07 19:48:27 -0400

    Fuck toString

--- pages/BlinkingCursor.tsx
+++ pages/BlinkingCursor.tsx
@@ -15,17 +15,17 @@ export default function BlinkingCursor(props: ThisComponenetsProps) {
   // useEffect(() => {
   //   console.log(props.cursorposx.toString() + 'ch');
   // }, [props.cursorposx]);
-  let cursorPosition = '0ch';
-  if (props.cursorposx && props.cursorposx != undefined && props.cursorposx != null) {
-    cursorPosition = props.cursorposx.toString() + 'ch';
-  }
+  // let cursorPosition = '0ch';
+  // if (props.cursorposx && props.cursorposx != undefined && props.cursorposx != null) {
+  //   cursorPosition = props.cursorposx.toString() + 'ch';
+  // }
 
 
   return (
     <>
       <span style={{
         position: 'absolute',
-        marginLeft: cursorPosition,
+        marginLeft: props.cursorposx + 'ch',
         borderLeft: '2px solid',
         borderColor: colorScheme === 'light' ? 'black' : 'white',
         height: '1.5em',
